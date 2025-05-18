27d2165cdafe8cf9d22364b899eb49817455485b
alexandre rabello <alerabello95@hotmail.com>
2025-05-18 16:33:18 -0300

    add is_admin = db.Column(db.Boolean, default=False)

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Usuario(UserMixin, db.Model):
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(200), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False) 
 
 class Cliente(db.Model):
     __tablename__ = 'clientes'
